d0fd1fb9b52d59e726eae866bcd5e27b4429fa7a
Joey Palchak <joey.palchak@gmail.com>
2023-11-13 18:24:48 -0800

    add conditional rendering + event handling

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,14 +7,29 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false // our default state: show the tickets, not the form
     };
   }
 
+  handleClick = () => {
+    this.setState({formVisibleOnPage: true});
+  }
+
+  // Conditional Rendering for our TicketList/NewTicketForm:
   render() {
+    let currentlyVisibleState = null;
+    let addTicketButton = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+      addTicketButton = <button onClick={this.handleClick}>Add Ticket</button>
+    }
+
     return(
       <React.Fragment>
-
+        {currentlyVisibleState}
+        {addTicketButton}
       </React.Fragment>
     );
   }
