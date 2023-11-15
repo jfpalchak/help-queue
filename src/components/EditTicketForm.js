536b5eaa2c0e27bb7b8b4041916ce1550cac8862
Joey Palchak <joey.palchak@gmail.com>
2023-11-15 13:58:47 -0800

    add form submission handler to EditTicketForm

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -3,9 +3,26 @@ import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
 function EditTicketForm(props) {
+
+  const { ticket } = props;
+
+  // function captures the form values on the form submit event,
+  // and simultaneously triggers the handleEditingTicketInList method from TicketControl
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    // utilize the event handler method passed in as a prop:
+    props.onEditTicket({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: ticket.id
+    });
+  }
+
   return (
     <React.Fragment>
       <ReusableForm 
+        formSubmissionHandler={handleEditTicketFormSubmission}
         buttonText="Update Ticket"
       />
     </React.Fragment>
@@ -13,7 +30,8 @@ function EditTicketForm(props) {
 }
 
 EditTicketForm.propTypes = {
-  ticket: PropTypes.object
+  ticket: PropTypes.object,
+  onEditTicket: PropTypes.func
 };
 
 export default EditTicketForm;
