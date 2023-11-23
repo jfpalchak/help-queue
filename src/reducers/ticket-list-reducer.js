9a789dd49bbf365e6babdb57db7b7f247c7c895f
Joey Palchak <joey.palchak@gmail.com>
2023-11-22 17:54:52 -0800

    pass second test for ticket-list-reducer

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,5 +1,25 @@
+// Our reducer is a pure function, 
+// not concerned about storing state, or mutating state.
+// It is only responsible for handling specific actions.
+
 const reducer = (state = {}, action) => {
-  return state;
+  const { names, location, issue, id } = action;
+
+  switch(action.type) {
+    case 'ADD_TICKET':
+      // We clone the given state object, add a new ticket to the clone,
+      // and we return that altered clone of the state object.
+      return Object.assign({}, state, {
+        [id] : {
+          names: names,
+          location: location,
+          issue: issue,
+          id: id
+        }
+      });
+    default:
+        return state;
+  }
 };
 
 export default reducer;
