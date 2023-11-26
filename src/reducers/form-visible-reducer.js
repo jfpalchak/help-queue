2f8471dbcb1efcfb88999bb881ff53a0f7692d30
Joey Palchak <joey.palchak@gmail.com>
2023-11-26 14:56:00 -0800

    refactor formVisibleOnPageReducer with createSlice

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 import * as c from './../actions/ActionTypes';
 
 const reducer = (state = false, action) => {
@@ -9,4 +11,29 @@ const reducer = (state = false, action) => {
   }
 };
 
-export default reducer;
+
+// RTK MIGRATION to createSlice
+
+// createSlice uses a library called Immer, which uses a JS tool
+// called a Proxy to wrap the data we're providing and let us 
+// write code that "mutates" that wrapped data. But Immer tracks
+// those changes and uses the list of changes to return a safely
+// immutably updated value, as if we've written all the immutable update logic by hand.
+
+// We can ONLY write this "mutating" esque logic in RTK's createSlice and createReducer,
+// because they're using Immer
+
+const formSlice = createSlice({
+  name: 'FORM',
+  initialState: { visible: false },
+  reducers: {
+    formToggled: (state) => {
+      state.visible = !state.visible
+    }
+  }  
+});
+
+export const { formToggled } = formSlice.actions;
+export default formSlice.reducer;
+
+// export default reducer;
