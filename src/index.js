93adbc3f305fd22081e86da8e275bca1060a4b54
Joey Palchak <joey.palchak@gmail.com>
2023-11-25 16:50:25 -0800

    refactor app entry point to import root reducer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 // Redux Imports:
 import { legacy_createStore as createStore } from 'redux';
-import reducer from './reducers/ticket-list-reducer';
+// import reducer from './reducers/ticket-list-reducer';
+import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 // For dev purposes:
 store.subscribe( () => 
