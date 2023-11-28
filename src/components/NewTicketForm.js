0947500f1ca659618e91eb92a9f6c1519fc67310
Joey Palchak <joey.palchak@gmail.com>
2023-11-28 09:24:54 -0800

    add timeOpen and WaitTime properties to NewTicket

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -2,18 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from "uuid";
 import ReusableForm from "./ReusableForm";
+import { formatDistanceToNow } from 'date-fns';
 
 function NewTicketForm(props) {
 
-  // this method handles the Submit event for our form:
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    // we utilize the method we passed through as a prop
-    // to update the parent's state with our new ticket
+
     props.onNewTicketCreation({
       names: event.target.names.value,
       location: event.target.location.value,
       issue: event.target.issue.value,
+      timeOpen: new Date(),
+      formattedWaitTime: formatDistanceToNow(new Date(), { addSuffix: true }),
       id: v4() // we create a unique id with the UUID library
     });
   }
