efb41ffe5258b47727c5a1677d14871c083acf12
Joey Palchak <joey.palchak@gmail.com>
2023-12-03 19:48:06 -0800

    install & play with styled-components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.png";
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HelpQueueHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: white;
+  background-color: purple;
+`;
+
+const StyledWrapper = styled.section`
+  background-color: orange;
+  padding-bottom: 15px;
+`;
 
 function Header() {
   return (
-    <header>
-      <h1>Help Queue
-        <img src={ticketsImage} alt="A couple of tickets." />
-      </h1>
+    <StyledWrapper>
+      <HelpQueueHeader>
+        <h1>Help Queue
+          <img src={ticketsImage} alt="A couple of tickets." />
+        </h1>
+      </HelpQueueHeader>
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -16,7 +31,7 @@ function Header() {
           <Link to="/sign-in">Sign In</Link>
         </li>
       </ul>
-    </header>
+    </StyledWrapper>
   );
 }
 
