75915c9f32563233b4fdaf28df50358c60deb475
Joey Palchak <joey.palchak@gmail.com>
2023-12-03 18:34:57 -0800

    add serverTimestamp to NewTicketForm

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,16 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
+import { serverTimestamp } from 'firebase/firestore';
+
 
 function NewTicketForm(props) {
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-
     props.onNewTicketCreation({
       names: event.target.names.value,
       location: event.target.location.value,
-      issue: event.target.issue.value
+      issue: event.target.issue.value,
+      timeOpen: serverTimestamp()
     });
   }
 
