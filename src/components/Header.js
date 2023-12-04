502c04d779a4b80d7760503bdae41839ca766162
Joey Palchak <joey.palchak@gmail.com>
2023-12-03 16:02:20 -0800

    implement Link with Header route navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ticketsImage from "./../img/tickets.png";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
+    <header>
       <h1>Help Queue
         <img src={ticketsImage} alt="A couple of tickets." />
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+      </ul>
+    </header>
   );
 }
 
