41778e5cddc2db1c085dfee388a3e6bdd749c36d
Joey Palchak <joey.palchak@gmail.com>
2023-12-03 16:21:32 -0800

    add form to SignIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,26 @@
 import React from "react";
 
 function SignIn() {
+
+  const doSignUp = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <h1>Sign In</h1>
+    <> 
+      <h1>Sign In</h1>
+      <form onSubmit={doSignUp}>
+        <input 
+          type="text"
+          name="email"
+          placeholder="Email" />
+        <input 
+          type="password"
+          name="password"
+          placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </>
   );
 }
 
